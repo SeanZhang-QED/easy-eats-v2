211bb5891fe01f9432e76ae89f2aeab7ef5eb656
SeanZhang-QED <66594541+SeanZhang-QED@users.noreply.github.com>
2021-12-08 01:30:32 -0800

    4th commit: implements signup form.

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,21 +4,57 @@ import {
     Box, Button,
     CircularProgress,
     Container,
-    Divider, IconButton,
+    Divider, Grid, IconButton,
     InputAdornment,
     TextField,
     ThemeProvider,
     Typography
 } from "@mui/material";
 import theme from "../theme";
-import {Link, Navigate} from "react-router-dom";
+import {Link, Navigate, useNavigate} from "react-router-dom";
 import Message from "./Message";
 import {Visibility, VisibilityOff} from "@mui/icons-material";
+import {signup} from "../utils/apis";
 
 function Register(props) {
-    const [ isSubmitting, setIsSubmitting ] = useState(false);
-    const [ isError, setIsError ] = useState(false);
-    const [ errorMessage, setErrorMessage ] = useState();
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState();
+    const [isMatch, setIsMatch] = useState(true);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        setIsSubmitting(true);
+
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const signupData = {
+            "email": data.get("email"),
+            "password": data.get("password"),
+            "firstName": data.get("firstName"),
+            "lastName": data.get("lastName"),
+        }
+
+        if (signupData.password !== data.get("confirm_password")) {
+            setIsMatch(false);
+            return;
+        }
+        signup(signupData)
+            .then(()=>{
+                navigate('/login');
+            })
+            .catch((err)=>{
+                setIsError(true);
+                // console.log(err);
+                setErrorMessage('Fail to Sign up');
+                console.log("Signup failed: ", err.message);
+                setIsSubmitting(false);
+            });
+
+    };
 
     return (
         <ThemeProvider theme={theme}>
@@ -35,7 +71,7 @@ function Register(props) {
                 }}/>
                 <Container component="main" maxWidth='xs'
                            sx={{
-                               height: '480px',
+                               height: '560px',
                                display: 'inline-block',
                                position: 'relative',
                                top: '30%',
@@ -54,16 +90,134 @@ function Register(props) {
                             textAlign: 'center',
                         }}
                     >
-                        <Typography variant="h5" gutterBottom marked="center" align="center" color='secondary.dark'
+                        <Typography variant="h5" gutterBottom marked="center" align="center" color='warning.dark'
                                     sx={{fontWeight: 'bold', mt: 4}}>
                             Sign Up and Enjoy!
                         </Typography>
                         <Divider variant='middle'>
                             <Typography variant="body2" align="center">
-                                {'Sign in with Email address as Username'}
+                                {'Sign up with Email address as Username'}
                             </Typography>
                         </Divider>
-                    {/* TODO: Add the register form body */}
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        autoComplete="given-name"
+                                        name="firstName"
+                                        fullWidth
+                                        id="firstName"
+                                        label="First Name"
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        fullWidth
+                                        id="lastName"
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="family-name"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        placeholder="Email Address/Username"
+                                        autoFocus
+                                        error={isEmpty}
+                                        helperText={isEmpty ? 'Can not leave it empty' : ''}
+                                        onChange={(e) => {
+                                            if (!e.target.value) {
+                                                setIsEmpty(true);
+                                            } else {
+                                                setIsEmpty(false);
+                                            }
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        error={!isMatch}
+                                        helperText={isMatch ? "" : "The two passwords you typed do not match. "}
+                                        onChange={() => {
+                                            setIsMatch(true)
+                                        }}
+                                        name="password"
+                                        label="Password"
+                                        type={showPassword ? "text" : "password"}
+                                        id="password"
+                                        autoComplete="new-password"
+                                        InputProps={{ // <-- This is where the toggle button is added.
+                                            endAdornment: (
+                                                <InputAdornment position="end">
+                                                    <IconButton
+                                                        onClick={() => setShowPassword(!showPassword)}
+                                                        onMouseDown={() => setShowPassword(!showPassword)}
+                                                    >
+                                                        {showPassword ? <Visibility/> : <VisibilityOff/>}
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            )
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="confirm_password"
+                                        label="Confirm Password"
+                                        error={!isMatch}
+                                        helperText={isMatch ? "" : "The two passwords you typed do not match. "}
+                                        placeholder="Please enter above password again."
+                                        type={"password"}
+                                        id="confirm_password"
+                                        InputProps={{
+                                            endAdornment: (
+                                                <InputAdornment position="end">
+                                                    <IconButton
+                                                        disabled={true}
+                                                    >
+                                                        <VisibilityOff/>
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            )
+                                        }}
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{
+                                    mt: 3, mb: 2, bgcolor: 'warning.dark',
+                                    ':hover': {bgcolor: '#D84315'}
+                                }}
+                            >
+                                Sign Up
+                            </Button>
+                            <Divider/>
+                            <Grid container justifyContent="center">
+                                <Grid item sx={{mt: 4, mb: 4}}>
+                                    <Link to="/login" style={{
+                                        textDecoration: 'none',
+                                        color: 'black',
+                                        fontSize: 14,
+                                        fontFamily: "'Roboto Condensed', sans-serif",
+                                    }}>
+                                        {'Already have an account? Sign in'}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
 
                     </Box>
                 </Container>
--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,1 +1,21 @@
 import axios from "axios";
+export const signup = (data) => {
+    const signupUrl = "/signup";
+    const opt ={
+        method: "POST",
+        url: signupUrl,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        // in fetch() is body, in axios is data
+        data: JSON.stringify(data),
+    };
+
+    console.log(opt.data);
+
+    return axios(opt).then((response) => {
+        if (response.status < 200 || response.status >= 300) {
+            throw Error("Fail to sign up");
+        }
+    });
+};
